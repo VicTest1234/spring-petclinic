becc3d7c474734a844f07a1c7feb35a3d24e6b7f
Victor Rosenberg <victor.rosenberg@myheritage.com>
2023-09-06 18:03:30 +0300

    PUll request

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -44,7 +44,7 @@ public class PetClinicApplication {
 	}
 
 	public void vicNewFunction222() {
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 30; i++) {
 			int x = ThreadLocalRandom.current().nextInt();
 			System.out.println("Vic lalala untested " + x);
 		}
